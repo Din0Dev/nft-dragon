1059dc2e96438a1cb4f032d02d825708c55cf126
Din0Dev <honghai251.info@gmail.com>
2021-12-10 15:54:13 +0700

    Update-Path-Css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,7 @@ import DefaultLayout from "layout/DefaultLayout";
 import "nprogress/nprogress.css";
 import 'antd/dist/antd.css';
 import "../styles/styles.scss";
-import "../styles/example.css";
-
+import "../styles/globals.css";
 
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
