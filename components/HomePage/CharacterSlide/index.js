541c312c189e50d37973d621ff6de2fa77fd54ea
DinoDev <honghai251.info@gmail.com>
2021-12-28 16:33:01 +0700

    Update Char5 Img

--- components/HomePage/CharacterSlide/index.js
+++ components/HomePage/CharacterSlide/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import Slider from "react-slick";
 
 const CharacterSlide = () => {
+       
   //! State
-
   var settings = {
     dots: false,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
@@ -15,34 +15,36 @@ const CharacterSlide = () => {
     initialSlide: 0,
     responsive: [
         {
-          breakpoint: 1290,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          }
+            breakpoint: 1290,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
         },
         {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+            }
         },
         {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-      ]
-  };
+    ]
+};
 
+  
+ 
   //! Render
   return (
     <div className="mx-4 md:mx-10 my-12 mt-0">
-      <div className="container max-w-screen-xl m-auto">
+      <div className="container max-w-screen-md m-auto">
         <Slider {...settings}>
           {/*  */}
           <div className="m-auto h-full col-span-1 pt-4 w-full">
@@ -98,6 +100,23 @@ const CharacterSlide = () => {
           <div className="m-auto h-full col-span-1 pt-4 w-full">
             <div className="mx-4 md:mx-1">
               <div className="rounded-lg border-4 lg:border-8 overflow-hidden h-full border-blue2 p-0 mt-12 md:mt-16 flex justify-center items-end home-card-dragon bg-center bg-no-repeat bg-cover bg-char4">
+                <div className="text-white text-center py-6 md:py-8 w-full px-4 bg-blur-black">
+                  <h3 className="text-xl text-white md:text-3xl font-bold">
+                    Tournament
+                  </h3>
+                  <div className="mt-1.5" style={{ minHeight: "48px" }}>
+                    <p>
+                      PvP battle to earn real <br /> money rewards
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+           {/*  */}
+           <div className="m-auto h-full col-span-1 pt-4 w-full">
+            <div className="mx-4 md:mx-1">
+              <div className="rounded-lg border-4 lg:border-8 overflow-hidden h-full border-blue2 p-0 mt-12 md:mt-16 flex justify-center items-end home-card-dragon bg-center bg-no-repeat bg-cover bg-char5">
                 <div className="text-white text-center py-6 md:py-8 w-full px-4 bg-blur-black">
                   <h3 className="text-xl text-white md:text-3xl font-bold">
                     Tournament
